acb514dcbfe9c353bc5e14808334564b2bde5ff4
Corynne Moody <cmoody@elevenfifty.org>
2021-06-26 16:40:53 -0400

    made some huge progress today, added bullets, aliens, and now if a bullet hits an alien it is deleted

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,35 +3,42 @@ import sys
 import pygame
 
 from settings import Settings
+from alien import Alien
 from ship import Ship 
 import game_functions as gf
+from pygame.sprite import Group 
+
 
 
 
 def run_game():
-    #Initialize game and create a screen object.
+    # Initialize game and create a screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_width)
+        (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    alien = Alien(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #Start the main loop for the game. 
     while True: 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         #Watch the keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        
-        #Make the most recently drawn screen visible
-        pygame.display.flip()
+  
 
 run_game()
+
+
+
--- settings.py
+++ settings.py
@@ -10,3 +10,15 @@ class Settings():
 
     # Ship settings
         self.ship_speed_factor = 1.5
+
+    # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+    # Alien settings
+        self.alien_speed_factor = float(0.1)
+        self.fleet_drop_speed = 1
+        self.fleet_direction = 1
